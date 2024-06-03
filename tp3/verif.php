dd1e1acf23f4030b26f9df08625f32ed059b24d0
OZn <153720754+nexzzoo@users.noreply.github.com>
2024-06-03 16:23:41 +0200

    Update verif.php

--- tp3/verif.php
+++ tp3/verif.php
@@ -1,1 +1,32 @@
+<?php
 
+session_start();
+
+
+if(isset($_POST['login']) && isset($_POST['psw'])) {
+    
+    $login = htmlspecialchars($_POST['login']);
+    $password = htmlspecialchars($_POST['psw']);
+    
+    
+    $valid_login = "utilisateur";
+    $valid_password = "motdepasse";
+
+    if($login === $valid_login && $password === $valid_password) {
+        
+        $_SESSION['login'] = $login;
+        
+        
+        header("Location: home.php");
+        exit();
+    } else {
+
+        $_SESSION['error'] = "Login incorrect";
+        header("Location: login.php");
+        exit();
+    }
+} else {
+    header("Location: login.php");
+    exit();
+}
+?>
